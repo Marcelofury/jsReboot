2bfaaaca7d3342eb3f8de35d852397faeb7abc27
Marcelofury <marcelofury13579@gmail.com>
2026-01-25 18:49:13 +0300

    added for loop example

---
+++ Arrays & Objects/loopingThroughArrays.js
@@ -0,0 +1,12 @@
+const fruits = ['apple', 'banana', 'mango'];
+
+// for loop  (classic)
+
+for (let i = 0; i < fruits.length; i++){
+    console.log(fruits[i]);
+}
+
+// for ...of loop (clean and modern)
+//for(const fruit of fruits){
+  //  console.log(fruit);
+//}
