150d25fabe1940caa0d7205d00b08c6abdacfe67
Marcelofury <marcelofury13579@gmail.com>
2026-01-25 19:08:11 +0300

    added map() arrayMethod

--- 03-Arrays & Objects/arrayMethods.js
+++ 03-Arrays & Objects/arrayMethods.js
@@ -1,1 +1,11 @@
 //this is where js pro starts
+
+//map ---- transforms data
+//creates a new array by applying a function to each element
+
+const numbers = [1, 2, 3, 4];
+
+const squared = numbers.map( num => num * num);
+console.log(squared);
+
+//map never changes the orriginal array
