9148cb9e47c7ddc933104c65bdf272d9c112ef09
Marcelofury <marcelofury13579@gmail.com>
2026-01-25 17:59:43 +0300

    added .length array operation

---
+++ Arrays & Objects/arrays.js
@@ -0,0 +1,14 @@
+const fruits = ['apple', 'banana', 'mango'];
+
+// arrays can store anything
+
+const mixed = [
+    'Marcel',
+    20,
+    true,
+    {role: 'Student'},
+    [1,2,3]
+];
+
+// common array operations
+fruits.length;
