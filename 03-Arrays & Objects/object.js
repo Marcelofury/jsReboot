326497dcba70d86010ad722f24420106823ff95b
Marcelofury <marcelofury13579@gmail.com>
2026-01-27 17:29:16 +0300

    added new property example

---
+++ 03-Arrays & Objects/object.js
@@ -0,0 +1,12 @@
+const user = {
+    name : 'Marcel',
+    age: 22,
+    role: 'student'
+};
+
+console.log(user)
+
+//Adding,Updating and deleting Properties
+// Add new property
+user.height = 180;
+user.age = 23
