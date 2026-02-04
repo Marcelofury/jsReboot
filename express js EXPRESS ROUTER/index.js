495bbe8327427761297ffc26e36db57520328123
Marcelofury <marcelofury13579@gmail.com>
2026-02-04 15:22:45 +0300

    installed express js

--- express js EXPRESS ROUTER/index.js
+++ express js EXPRESS ROUTER/index.js
@@ -6,3 +6,18 @@ app.use(express.json()); // Middleware to parse JSON bodies
 const userRoutes = require('./routes/users'); // Importing the user routes
 
 app.use('/users', userRoutes); // Using the user routes with /users prefix
+
+app.listen(3000, () => {
+    console.log('server running on port 3000');
+});
+
+
+    // resulting endpoints
+
+   // GET /users
+   // POST /users
+   // GET /users/:id
+   // PUT /users/:id
+   // DELETE /users/:id
+
+   // This is a clean REST architecture 
