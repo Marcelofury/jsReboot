93b16e4df6383beba85e3c6f8a6bcdec1c219a32
Marcelofury <marcelofury13579@gmail.com>
2026-02-04 14:53:11 +0300

    added POST route

--- express js EXPRESS ROUTER/routes/users.js
+++ express js EXPRESS ROUTER/routes/users.js
@@ -1,11 +1,14 @@
 
 // initialization
 const express = require('express');
-const app = express();
+const router = express.Router();
 
 // creating routes
 router.get('/', (req, res) => {
     res.json({message : 'Get all users'});
 });
 
+router.post('/', (req, res) => {
+    res.json({message : 'cretae user'})
+});
 
