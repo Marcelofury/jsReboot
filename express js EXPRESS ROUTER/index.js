8d51cf26d67a1c8a70428ec39fcdbf2059425d99
Marcelofury <marcelofury13579@gmail.com>
2026-02-04 15:05:24 +0300

    imported user routes

--- express js EXPRESS ROUTER/index.js
+++ express js EXPRESS ROUTER/index.js
@@ -2,3 +2,5 @@ const express = require('express');
 const app = express();
 
 app.use(express.json()); // Middleware to parse JSON bodies
+
+const userRoutes = require('./routes/users'); // Importing the user routes
