c8e9966ea80c86fe53233adf7c5ec33f47cc2ba9
Marcelofury <marcelofury13579@gmail.com>
2026-02-04 14:51:29 +0300

    added GET route

--- express js EXPRESS ROUTER/routes/users.js
+++ express js EXPRESS ROUTER/routes/users.js
@@ -1,2 +1,11 @@
+
+// initialization
 const express = require('express');
 const app = express();
+
+// creating routes
+router.get('/', (req, res) => {
+    res.json({message : 'Get all users'});
+});
+
+
