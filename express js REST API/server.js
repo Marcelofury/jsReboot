9332960a2bba2f0f08ba8b2ae8726b03f73e72f2
Marcelofury <marcelofury13579@gmail.com>
2026-02-04 14:11:12 +0300

    defined a route for GET requests

--- express js REST API/server.js
+++ express js REST API/server.js
@@ -4,3 +4,8 @@ const app = express();
 const port = 3000;
 
 app.use(express.json()); // Middleware to parse JSON bodies
+
+// Define a route for GET requests
+app.get('users', (req,res)=>{
+    res.json({message : 'returning list of users'});
+});
